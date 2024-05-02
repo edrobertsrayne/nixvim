9d1d33adc6388c1632340896bd3817e8fa31a7a7
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-02 22:51:08 +0100

    feat: add bufferlines plugin and keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -8,39 +8,6 @@
       options.desc = "New file";
     }
 
-    # Buffer navigation
-
-    {
-      key = "<leader>`";
-      action = "<cmd>e #<cr>";
-      options.desc = "Switch to other buffer";
-    }
-    {
-      key = "<leader>bb";
-      action = "<cmd>e #<cr>";
-      options.desc = "Switch to other buffer";
-    }
-    {
-      key = "<leader>[b";
-      action = "<cmd>bprevious<cr>";
-      options.desc = "Switch to previous buffer";
-    }
-    {
-      key = "<leader>bp";
-      action = "<cmd>bprevious<cr>";
-      options.desc = "Switch to previous buffer";
-    }
-    {
-      key = "<leader>bn";
-      action = "<cmd>bnext<cr>";
-      options.desc = "Switch to next buffer";
-    }
-    {
-      key = "<leader>]b";
-      action = "<cmd>bnext<cr>";
-      options.desc = "Switch to next buffer";
-    }
-
     # Indenting
 
     {
