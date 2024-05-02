e1248d7e544f0cb2520fe92384c6943e3bf62f6c
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-02 22:29:03 +0100

    feat: add buffer, terminal, new file and indenting keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,2 +1,66 @@
 {
+  keymaps = [
+    # Files
+
+    {
+      key = "<leader>fn";
+      action = "<cmd>enew<cr>";
+      options.desc = "New file";
+    }
+
+    # Buffer navigation
+
+    {
+      key = "<leader>`";
+      action = "<cmd>e #<cr>";
+      options.desc = "Switch to other buffer";
+    }
+    {
+      key = "<leader>bb";
+      action = "<cmd>e #<cr>";
+      options.desc = "Switch to other buffer";
+    }
+    {
+      key = "<leader>[b";
+      action = "<cmd>bprevious<cr>";
+      options.desc = "Switch to previous buffer";
+    }
+    {
+      key = "<leader>bp";
+      action = "<cmd>bprevious<cr>";
+      options.desc = "Switch to previous buffer";
+    }
+    {
+      key = "<leader>bn";
+      action = "<cmd>bnext<cr>";
+      options.desc = "Switch to next buffer";
+    }
+    {
+      key = "<leader>]b";
+      action = "<cmd>bnext<cr>";
+      options.desc = "Switch to next buffer";
+    }
+
+    # Indenting
+
+    {
+      key = "<";
+      action = "<gv";
+      mode = "v";
+    }
+    {
+      key = ">";
+      action = ">gv";
+      mode = "v";
+    }
+
+    # Terminal
+
+    {
+      key = "<esc><esc>";
+      action = "<c-\\><c-n>";
+      options.desc = "Enter normal mode";
+      mode = "t";
+    }
+  ];
 }
