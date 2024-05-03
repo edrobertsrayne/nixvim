d1a391979fb9bcd3e554765d9cc729a6a375e1a6
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-03 12:08:59 +0100

    feat: add illuminate plugin

--- config/utils/illuminate.nix
+++ config/utils/illuminate.nix
@@ -1,1 +1,17 @@
-{}
+{
+  plugins.illuminate = {
+    enable = true;
+    underCursor = false;
+    filetypesDenylist = [
+      "DressingSelect"
+      "Outline"
+      "TelescopePrompt"
+      "alpha"
+      "harpoon"
+      "toggleterm"
+      "neo-tree"
+      "Spectre"
+      "reason"
+    ];
+  };
+}
