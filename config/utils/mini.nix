f6956569aa5dfc8b4bc18a8ed0fde68c32289501
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-03 17:30:47 +0100

    feat: add mini.nvim modules ai, animate, basics, pairs, surround

--- config/utils/mini.nix
+++ config/utils/mini.nix
@@ -1,4 +1,29 @@
 {
   plugins.mini = {
+    enable = true;
+    modules = {
+      animate = {};
+      ai = {};
+      basics = {
+        options = {
+          basic = true;
+          extra_ui = false;
+          win_borders = "default"; # single, double, default...
+        };
+        mappings = {
+          basic = true;
+          option_toggle_prefix = ''[[\]]'';
+          windows = true;
+          move_with_alt = false;
+        };
+        autocommands = {
+          basic = true;
+          relnum_in_visual_mode = false;
+        };
+        silent = false;
+      };
+      pairs = {};
+      surround = {};
+    };
   };
 }
