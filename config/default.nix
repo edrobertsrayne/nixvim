bfbf76edd103da77c1376ca9b3126be0864318c8
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-03 02:04:39 +0100

    MRE for alpha bug

--- config/default.nix
+++ config/default.nix
@@ -1,35 +1,4 @@
 {
-  imports = [
-    ./keymaps.nix
-
-    ./bufferlines/bufferline.nix
-    ./completion/cmp.nix
-    ./filetrees/neo-tree.nix
-    ./git/lazygit.nix
-    ./git/gitsigns.nix
-    ./lsp
-    ./none-ls
-    ./snippets/luasnip.nix
-    ./statuslines/lualine.nix
-    ./telescope
-    ./treesitter
-    ./ui/nui.nix
-    ./ui/noice.nix
-    ./ui/alpha.nix
-    ./utils/oil.nix
-    ./utils/nix.nix
-    ./utils/mini.nix
-    ./utils/flash.nix
-    ./utils/notify.nix
-    ./utils/which-key.nix
-    ./utils/illuminate.nix
-    ./utils/persistence.nix
-    ./utils/todo-comments.nix
-    ./utils/tmux-navigator.nix
-    ./utils/indent-blankline.nix
-  ];
-
-  colorschemes.catppuccin.enable = true;
-
-  globals.mapleader = " ";
+  plugins.alpha = { enable = true; 
+theme = "vim-startify-theme";};
 }
