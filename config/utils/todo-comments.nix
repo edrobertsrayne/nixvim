b54ea88fda6845dd8cbecd6a8d3c7a7d943d6060
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-08 23:12:46 +0100

    feat: add todo-comments and keymaps

--- config/utils/todo-comments.nix
+++ config/utils/todo-comments.nix
@@ -1,1 +1,37 @@
-{}
+{
+  plugins.todo-comments = {
+    enable = true;
+  };
+  keymaps = [
+    {
+      action = ''lua require("todo-comments").jump_next()<cr>'';
+      key = "]t";
+      options.desc = "Next Todo Comment";
+    }
+    {
+      action = ''lua require("todo-comments").jump_prev()<cr>'';
+      key = "[t";
+      options.desc = "Previous Todo Comment";
+    }
+    {
+      action = "<cmd> TodoTrouble <cr>";
+      key = "<leader>xt";
+      options.desc = "Todo (Trouble)";
+    }
+    {
+      action = "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>";
+      key = "<leader>xT";
+      options.desc = "Todo/Fix/Fixme (Trouble)";
+    }
+    {
+      action = "<cmd> TodoTelescope <cr>";
+      key = "<leader>st";
+      options.desc = "Todo (Telescope)";
+    }
+    {
+      action = "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>";
+      key = "<leader>sT";
+      options.desc = "Todo/Fix/Fixme (Telescope)";
+    }
+  ];
+}
