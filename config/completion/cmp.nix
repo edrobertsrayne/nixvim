dacfaee075c81790183983d26474c0eaceb263cd
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-15 09:13:13 +0100

    feat(cmp): enable spell, latex-symbols, cmdline completion

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -54,7 +54,9 @@
     cmp-buffer = {enable = true;};
     cmp-path = {enable = true;}; # file system paths
     cmp_luasnip = {enable = true;}; # snippets
-    cmp-cmdline = {enable = false;}; # autocomplete for cmdline
+    cmp-cmdline = {enable = true;}; # autocomplete for cmdline
+    cmp-latex-symbols = {enable = true;};
+    cmp-spell = {enable = true;};
   };
   extraConfigLua = ''
           luasnip = require("luasnip")
